a93a448971e438330c8beb4db989b3b7da4dfc2f
ultimate <ultimatej@gmx.net>
2021-01-07 11:22:54 +0100

    added web.xml and jetty context.xml

--- src/main/webapp/scripts/View.js
+++ src/main/webapp/scripts/View.js
@@ -107,13 +107,13 @@ var View = function(container) {
 	};
 	
 	// TODO for debug only
-	
+	/*
 	var g = new THREE.CubeGeometry(100,100,100);
 	var m = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
 	var cube = new THREE.Mesh( g, m );
 	cube.name = "cube";
 	this.scene.add(cube);
-	
+	*/
 	// TODO end
 	
 	this.load = function(sectors, stepSize) {
